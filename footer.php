8b2c915dfebe558479012ed28fd26fd414b34748
ochkarius007-ux <ochkarius007@gmail.com>
2026-03-07 10:56:06 +0300

    Update footer.php
    Signed-off-by: ochkarius007-ux <ochkarius007@gmail.com>

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         © 2026 БФ "МУРМАНСК РАЗВИВАЕМ". Все права защищены.<br>
         <a href="index.php">Главная</a> |
         <a href="about.php">О фонде</a> |
-        <a href="programs.php">Программы</a> |
+        <a href="programs.php">Проекты</a> |
         <a href="news.php">Новости</a> |
         <a href="gallery.php">Фотоальбомы</a> |
         <a href="join.php">Стать участником</a>
