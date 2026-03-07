51846e7262dd119eb41f918608bb674050d33e92
ochkarius007-ux <ochkarius007@gmail.com>
2026-03-07 10:56:51 +0300

    Refactor image processing functions for brevity
    Signed-off-by: ochkarius007-ux <ochkarius007@gmail.com>

--- functions.php
+++ functions.php
@@ -1,14 +1,10 @@
 <?php
 function ensureDir($dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0755, true);
-    }
+    if (!is_dir($dir)) mkdir($dir, 0755, true);
 }
 
 function compressImage($source, $destination, $max_width = 1200, $quality = 80) {
-    if (!file_exists($source) || !function_exists('imagecreatefromjpeg')) {
-        return; // защита от 500
-    }
+    if (!file_exists($source)) return;
     list($width, $height, $type) = getimagesize($source);
     $ratio = $width / $height;
     if ($width > $max_width) {
@@ -16,21 +12,12 @@ function compressImage($source, $destination, $max_width = 1200, $quality = 80) 
         $height = (int)($width / $ratio);
     }
     $new_image = imagecreatetruecolor($width, $height);
-    
     switch ($type) {
-        case IMAGETYPE_JPEG:
-            $image = @imagecreatefromjpeg($source);
-            break;
-        case IMAGETYPE_PNG:
-            $image = @imagecreatefrompng($source);
-            break;
-        case IMAGETYPE_GIF:
-            $image = @imagecreatefromgif($source);
-            break;
-        default:
-            return;
+        case IMAGETYPE_JPEG: $image = @imagecreatefromjpeg($source); break;
+        case IMAGETYPE_PNG:  $image = @imagecreatefrompng($source); break;
+        case IMAGETYPE_GIF:  $image = @imagecreatefromgif($source); break;
+        default: return;
     }
-    
     if ($image) {
         imagecopyresampled($new_image, $image, 0, 0, 0, 0, $width, $height, imagesx($image), imagesy($image));
         imagejpeg($new_image, $destination, $quality);
@@ -42,6 +29,6 @@ function compressImage($source, $destination, $max_width = 1200, $quality = 80) 
 function getAlbums() {
     ensureDir('images/albums/');
     $folders = array_diff(scandir('images/albums/'), ['.', '..']);
-    return array_filter($folders, function($f) { return is_dir('images/albums/' . $f); });
+    return array_filter($folders, fn($f) => is_dir('images/albums/' . $f));
 }
 ?>
